7dab515dde439010b833db9dc55885db4d7a2308
lizhipeng <mail@lzp.name>
2017-03-29 15:44:55 +0800

    修改登录用户名密码为admin

--- apps/frontend/controllers/BaseController.php
+++ apps/frontend/controllers/BaseController.php
@@ -14,7 +14,7 @@ class BaseController extends Controller
             print "You need valid credentials to get access!\n";
             exit;
         } else {
-            if ($_SERVER['PHP_AUTH_USER'] !== 'mani' || $_SERVER['PHP_AUTH_PW'] !== 'w#m3nt0r') {
+            if ($_SERVER['PHP_AUTH_USER'] !== 'admin' || $_SERVER['PHP_AUTH_PW'] !== 'admin') {
                 header("WWW-Authenticate: Basic realm=\"My Private Area\"");
                 header("HTTP/1.0 401 Unauthorized");
                 print "You need valid credentials to get access!\n";
