f5595f157b4650d36b8f6ffec5dd185fc6ac1cd4
lizhipeng <mail@lzp.name>
2017-04-11 17:04:12 +0800

    添加显示列

--- apps/frontend/controllers/ApiController.php
+++ apps/frontend/controllers/ApiController.php
@@ -79,7 +79,7 @@ class ApiController extends BaseController
         if ($injuries) {
             // 保存操作
             $errors = [];
-            $fields = ['statusCn', 'commentCn'];
+            $fields = ['commentCn'];
             foreach ($fields as $field) {
                 $value = trim(htmlspecialchars($this->request->get($field)));
                 empty($value) && $errors[] = "请填写${field}字段";
